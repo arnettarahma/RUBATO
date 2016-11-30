f6d790300efaaeab555be6c18a224e3e857efa59
Grandis Rufaidah <grandisrufaidah99@gmail.com>
2016-11-29 21:19:09 -0800

    Navigasi Drawer

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl408172635/rubato/Fragment/AboutUs.java
+++
@@ -1,7 +0,0 @@
-package id.sch.smktelkom_mlg.project.xirpl408172635.rubato.Fragment;
-
-/**
- * Created by Smktelkomm on 11/28/2016.
- */
-public class AboutUs {
-}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl408172635/rubato/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl408172635/rubato/MainActivity.java
@@ -4,98 +4,76 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onNavigationItemSelected(MenuItem menuItem)
+            {
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_home:
+                        Toast.makeText(getApplicationContext(), "Home Telah Terpilih", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_aboutus:
+                        Toast.makeText(getApplicationContext(), "About Us Telah Terpilih", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_daftar:
+                        Toast.makeText(getApplicationContext(), "List Music Telah Terpilih", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_setting:
+                        Toast.makeText(getApplicationContext(), "Setting Telah Terpilih", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_profil:
+                        Toast.makeText(getApplicationContext(), "About Rubato Telah Terpilih", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Terjadi Kesalahan", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new
+                ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer)
+                {
+                    @Override
+                    public void onDrawerClosed(View drawerView)
+                    {
+                        super.onDrawerClosed(drawerView);
+                    }
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+                    @Override
+                    public void onDrawerOpened(View drawerView)
+                    {
+                        super.onDrawerOpened(drawerView);
+                    }
+                    };
+                    drawerLayout.setDrawerListener(actionBarDrawerToggle);
+                    actionBarDrawerToggle.syncState();
+                }
 }
+
